f4788ab6f28f713c75684c26720b913aca16a7c1
Mihaela <mihaellatocu@yahoo.com>
2025-04-01 18:21:24 +0100

    norminetted

--- src/exec/game_init.c
+++ src/exec/game_init.c
@@ -6,7 +6,7 @@
 /*   By: mtocu <mtocu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:30:00 by mtocu             #+#    #+#             */
-/*   Updated: 2025/03/27 15:00:00 by mtocu            ###   ########.fr       */
+/*   Updated: 2025/04/01 18:18:24 by mtocu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,20 +48,21 @@ void	init_textures(t_map *map)
 		map->img[i].image = mlx_xpm_file_to_image(map->mlx, map->img[i].path,
 				&map->img[i].width, &map->img[i].height);
 		if (!map->img[i].image)
-			handle_game_err(map,RED "Error\nPath texture is incorrect\n" RST);
+			handle_game_err(map, RED "Error\nPath texture is incorrect\n" RST);
 		i++;
 	}
 	map->img[4].image = mlx_new_image(map->mlx,
 			map->display_width, map->display_height);
 	if (!map->img[4].image)
-		handle_game_err(map, RED "Error\nImage initialisation has failed\n" RST);
+		handle_game_err(map, RED "Error\nImage initialisation failed\n" RST);
 	i = 0;
 	while (i < 5)
 	{
 		map->img[i].pixels = mlx_get_data_addr(map->img[i].image,
-				&map->img[i].bits_per_pixel, &map->img[i].line_size, &map->img[i].endian);
+				&map->img[i].bits_per_pixel, &map->img[i].line_size,
+				&map->img[i].endian);
 		if (!map->img[i].pixels)
-			handle_game_err(map,RED "Error\nImage initialisation has failed\n" RST);
+			handle_game_err(map, RED "Error\nImage initialisation fail\n" RST);
 		i++;
 	}
 }
@@ -111,11 +112,12 @@ void	init_minimap(t_map *map)
 	map->minimap.image = mlx_new_image(map->mlx,
 			map->width_map * 7, map->height_map * 7);
 	if (!map->minimap.image)
-		handle_game_err(map,RED "Error\nMinimap creation has failed\n" RST);
+		handle_game_err(map, RED "Error\nMinimap creation has failed\n" RST);
 	map->minimap.pixels = mlx_get_data_addr(map->minimap.image,
-			&map->minimap.bits_per_pixel, &map->minimap.line_size, &map->minimap.endian);
+			&map->minimap.bits_per_pixel, &map->minimap.line_size,
+			&map->minimap.endian);
 	if (!map->minimap.pixels)
-		handle_game_err(map,RED "Error\nMinimap creation has failed\n" RST);
+		handle_game_err(map, RED "Error\nMinimap creation has failed\n" RST);
 	draw_minimap(map);
 }
 
@@ -129,11 +131,11 @@ int	start_game(t_map *map)
 	check_game_init(map);
 	map->mlx = mlx_init();
 	if (!map->mlx)
-		handle_game_err(map,RED "Error\nInitialisation of display has failed\n" RST);
+		handle_game_err(map, RED "Error\nInitialisation display failed\n" RST);
 	map->mlx_win = mlx_new_window(map->mlx, map->display_width,
 			map->display_height, "Cub3D");
 	if (!map->mlx_win)
-		handle_game_err(map,RED "Error\nInitialisation of window has failed\n" RST);
+		handle_game_err(map, RED "Error\nInitialisation window failed\n" RST);
 	init_textures(map);
 	init_minimap(map);
 	mlx_loop_hook(map->mlx, &render_frame, map);
