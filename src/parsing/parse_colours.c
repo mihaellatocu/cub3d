eebfa0c684272f2a4862eab7661817c89cf14602
Mihaela <mihaellatocu@yahoo.com>
2025-04-01 19:59:22 +0100

    norminetted all

--- src/parsing/parse_colours.c
+++ src/parsing/parse_colours.c
@@ -6,7 +6,7 @@
 /*   By: mtocu <mtocu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 14:12:47 by mtocu             #+#    #+#             */
-/*   Updated: 2025/04/01 19:02:00 by mtocu            ###   ########.fr       */
+/*   Updated: 2025/04/01 19:57:24 by mtocu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,41 +45,18 @@ void	assign_texture_path(t_map *map, char **path, char **split_line)
 ** - Should not contain repeated commas or non-digit/non-comma characters.
 ** - Must contain exactly two commas and be under 12 characters in length.
 */
-int	parse_rgb(char *line)
+
+int	check_rgb_value(int *rgb)
 {
-	int	i;
-	int	comma;
-	int	prevWasComma;
 	int	k;
 
-	i = 0;
-	comma = 0;
-	prevWasComma = 0;
-	while (line[i] && ft_isspace(line[i]))
-		i++;
-	if (!ft_isdigit(line[i]))
-		return (0);
-	while (line[i])
+	k = -1;
+	while (++k < 3)
 	{
-		if (ft_isdigit(line[i]))
-			prevWasComma = 0;
-		else if (line[i] == ',')
-		{
-			comma++;
-			if (prevWasComma)
-				return (0);
-			prevWasComma = 1;
-		}
-		else if (!ft_isspace(line[i]))
-			return (0);
-		i++;
+		if (rgb[k] < 0 || rgb[k] > 255)
+			return (ft_printf("Error: RGB value %d out of range\n", rgb[k]), 0);
 	}
-	k = i - 1;
-	while (k >= 0 && ft_isspace(line[k]))
-		k--;
-	if (k >= 0 && !ft_isdigit(line[k]))
-		return (0);
-	return (comma == 2);
+	return (1);
 }
 
 /*
@@ -88,51 +65,29 @@ int	parse_rgb(char *line)
 ** - Verifies that each RGB value is between 0 and 255.
 ** - Returns 1 on success, 0 on invalid format or out-of-bounds values.
 */
-int	parse_rgb_value(int *rgb, char *line)
+int	parse_rgb_value(int *rgb, char *line, int i, int j)
 {
-	int	i;
-	int	j;
-	int	k;
-
-	i = 0;
-	j = 0;
 	while (j < 3)
 	{
 		while (line[i] && ft_isspace(line[i]))
 			i++;
 		if (!ft_isdigit(line[i]))
-		{
-			ft_printf("Error: expected digit at pos %d\n", i);
-			return (0);
-		}
+			return (ft_printf("Error: expected digit at pos %d\n", i), 0);
 		rgb[j] = ft_atoi(&line[i]);
 		while (line[i] && ft_isdigit(line[i]))
 			i++;
 		while (line[i] && ft_isspace(line[i]))
 			i++;
 		if (j < 2 && line[i++] != ',')
-		{
-			ft_printf("Error: expected comma at pos %d\n", i - 1);
-			return (0);
-		}
+			return (ft_printf("Error: expected comma at pos %d\n", i - 1), 0);
 		j++;
 	}
 	while (line[i] && ft_isspace(line[i]))
 		i++;
 	if (line[i])
-	{
-		ft_printf("Error: extra characters after RGB values: '%s'\n", &line[i]);
+		return (ft_printf("Error: extra char after RGB: '%s'\n", &line[i]), 0);
+	if (!check_rgb_value(rgb))
 		return (0);
-	}
-	k = -1;
-	while (++k < 3)
-	{
-		if (rgb[k] < 0 || rgb[k] > 255)
-		{
-			ft_printf("Error: RGB value %d out of range\n", rgb[k]);
-			return (0);
-		}
-	}
 	return (1);
 }
 
@@ -147,14 +102,48 @@ void	parse_rgb_map(t_map *map, int *rgb, char *line)
 {
 	char	*ptr;
 
-	// Find the first space after the identifier
 	ptr = ft_strchr(line, ' ');
 	if (!ptr)
 		parse_err(map, "Error\nRGB line is incorrect\n");
-	// Skip the space(s) to get to the RGB values
 	while (*ptr && ft_isspace(*ptr))
 		ptr++;
-	// Now, ptr should point to something like "90, 54, 67"
-	if (!parse_rgb_value(rgb, ptr))
+	if (!parse_rgb_value(rgb, ptr, 0, 0))
 		parse_err(map, "Error\nRGB line is incorrect\n");
 }
+
+// int	parse_rgb(char *line, int i, int comma, int prev_comma)
+// {
+// 	int	i;
+// 	int	comma;
+// 	int	prev_comma;
+// 	int	k;
+
+// 	i = 0;
+// 	comma = 0;
+// 	prev_comma = 0;
+// 	while (line[i] && ft_isspace(line[i]))
+// 		i++;
+// 	if (!ft_isdigit(line[i]))
+// 		return (0);
+// 	while (line[i])
+// 	{
+// 		if (ft_isdigit(line[i]))
+// 			prev_comma = 0;
+// 		else if (line[i] == ',')
+// 		{
+// 			comma++;
+// 			if (prev_comma)
+// 				return (0);
+// 			prev_comma = 1;
+// 		}
+// 		else if (!ft_isspace(line[i]))
+// 			return (0);
+// 		i++;
+// 	}
+// 	k = i - 1;
+// 	while (k >= 0 && ft_isspace(line[k]))
+// 		k--;
+// 	if (k >= 0 && !ft_isdigit(line[k]))
+// 		return (0);
+// 	return (comma == 2);
+// }
